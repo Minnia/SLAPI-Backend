b06d1ce90cb287effbe15062ea07e32bcea37300
Minnia <jasminestockholm@gmail.com>
2022-12-28 19:35:20 +0100

    module, schema and service for user

---
+++ src/services/user.service.ts
@@ -0,0 +1,23 @@
+import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from '../schemas/user.schema';
+import { CreateUserDto } from '../dtos/CreateUserDto';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async create(CreateUserDto: CreateUserDto): Promise<User> {
+    const createdUser = new this.userModel(CreateUserDto);
+    return createdUser.save();
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+
+  getUser(): Promise<User> {
+    return this.userModel.findOne().exec();
+  }
+}

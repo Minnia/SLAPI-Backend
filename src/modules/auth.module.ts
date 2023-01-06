179e0de40f11a7129f235a33bf0a72520673e0f9
Minnia <jasminestockholm@gmail.com>
2023-01-06 22:58:11 +0100

    rm module that isn't needed

--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { UserController } from '../controllers/user.controller';
 import { UserService } from '../services/user.service';
 
 @Module({
-  imports: [UserModule, MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [UserModule],
   controllers: [UserController],
   providers: [UserService],
 })

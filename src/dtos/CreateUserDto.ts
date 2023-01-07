e61a77d9ff3580cccc6e09a893d8c10467c27568
Minnia <jasminestockholm@gmail.com>
2023-01-07 01:23:35 +0100

    add validation to dto fields

--- src/dtos/CreateUserDto.ts
+++ src/dtos/CreateUserDto.ts
@@ -1,5 +1,13 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   firstName: string;
+  @IsString()
+  @IsNotEmpty()
   lastName: string;
+  @IsNumber()
+  @IsNotEmpty()
   age: number;
 }

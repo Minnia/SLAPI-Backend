b6cd62cea44fd4bb08ddd23566bd15ab75c43ce4
Minnia <jasminestockholm@gmail.com>
2023-01-07 01:23:23 +0100

    add new Get and Delete

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query, Delete } from '@nestjs/common';
 import { UserService } from '../services/user.service';
 import { User } from '../schemas/user.schema';
 import { CreateUserDto } from 'src/dtos/CreateUserDto';
@@ -11,8 +11,16 @@ export class UserController {
   createUser(@Body() user: CreateUserDto): Promise<User> {
     return this.userService.create(user);
   }
+  @Get('all-users')
+  getAllUsers(): Promise<User[]> {
+    return this.userService.findAll();
+  }
   @Get('user')
   getUser(@Query('id') id: string): Promise<User> {
     return this.userService.getUser(id);
+  }
+  @Delete('user-by-id')
+  deleteUser(@Query('id') id: string): Promise<User> {
+    return this.userService.deleteUser(id);
   }
 }

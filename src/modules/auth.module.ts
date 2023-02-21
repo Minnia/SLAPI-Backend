eb9fcde4bec68de0c122e33ebb8278a28df2f0dd
Minnia <jasmine.pishnemazi@redmind.se>
2023-02-21 02:24:03 +0100

    add modules to auth module

--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthController } from 'src/controllers/auth.controller';
+import { User, UserSchema } from 'src/schemas/user.schema';
+import { AuthService } from 'src/services/auth.service';
 import { UserController } from '../controllers/user.controller';
 import { UserService } from '../services/user.service';
+import { UserModule } from './user.module';
 
 @Module({
-  imports: [UserModule],
-  controllers: [UserController],
-  providers: [UserService],
+  imports: [JwtModule, UserModule, ConfigModule, MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),],
+  controllers: [UserController, AuthController],
+  providers: [UserService, AuthService, JwtService, ConfigService],
 })
-export class UserModule {}
+export class AuthModule {}
